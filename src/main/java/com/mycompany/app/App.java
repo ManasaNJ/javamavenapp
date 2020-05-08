0f66acb4ab353b1aeab981785744e5c98303600f
ManasaNJ <63852818+ManasaNJ@users.noreply.github.com>
2020-05-08 05:14:20 +0000

    Update App.java
    Modified display message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,12 @@
 package com.mycompany.app;
 
 /**
- * Hello world!
+ * Hello world!Have a Great day
  */
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World!Have a Great day";
 
     public App() {}
 
