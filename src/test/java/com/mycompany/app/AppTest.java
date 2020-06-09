327dba0973b9ee7c1d4b2e9b3244f8811b56847d
ManasaNJ <63852818+ManasaNJ@users.noreply.github.com>
2020-06-09 13:20:37 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World!Good Bye" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World!Good" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!Good Bye\"");
         }
